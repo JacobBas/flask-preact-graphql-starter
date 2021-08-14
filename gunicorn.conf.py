6cd7f4242f0c32e48cc70dc6a55c73633a8a2763
jacobbas <49172285+JacobBas@users.noreply.github.com>
2021-08-14 12:04:55 -0700

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,11 +1,10 @@
 # all available confirguations for a gunicorn server
 ### where do we want to host the application
-# bind = "0.0.0.0:17875"
-bind = "0.0.0.0:17888"
+bind = "0.0.0.0:4040"
 
 ### how performant do we want the application to be; want to find a good
 ### balance of performance and server capacity
-workers = 3
+workers = 1
 threads = 1
 
 ### assigning the ssl/https private and public key files
